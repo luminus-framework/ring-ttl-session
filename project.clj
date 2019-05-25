0d7dd9c1d093d1c2cdb980f5de7d29696a719386
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-05-25 11:34:19 -0400

    bumped up expring-map

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/ring-ttl-session "0.3.2"
+(defproject luminus/ring-ttl-session "0.3.3"
   :description "Provides an implementation of an in-memory ring's SessionStore with
                TTL."
   :url "https://github.com/luminus-framework/ring-ttl-session"
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [ring/ring-core "1.4.0"]
-                 [expiring-map "0.1.8"]]
+                 [expiring-map "0.1.9"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
   :aliases {"test" ["test" ":only" "ring-ttl-session.core-test"]}
   )
